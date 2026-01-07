be9e224a1b15474a2b9f6197db25ef88560b1490
Danil Babikov <116505393+DanilBabikov0@users.noreply.github.com>
2026-01-07 18:27:16 +0500

    Ready for deploy .1

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'danilbabikov.pythonanywhere.com']
 
 # Application definition
 
